7c04a328c5c3d58a4f88a0cafbbdc5934bc562f3
Constantine Lebedev <koss.lebedev@gmail.com>
2017-05-20 18:36:17 +0500

    Eslint fixes

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,26 @@
-var gulp = require('gulp'),
-    minify = require('gulp-minify'),
-    cleanCSS = require('gulp-clean-css'),
-    babel = require('gulp-babel');
+const gulp = require('gulp');
+const minify = require('gulp-minify');
+const cleanCSS = require('gulp-clean-css');
+const babel = require('gulp-babel');
 
-gulp.task('default', [ 'minify-js', 'minify-css' ]);
+gulp.task('default', ['minify-js', 'minify-css']);
 
 gulp.task('minify-js', function() {
-    gulp.src('src/*.js')
-        .pipe(babel({
-            presets: ['es2015']
-        }))
-        .pipe(minify({
-            ext:{
-                src:'-debug.js',
-                min:'.js'
-            }
-        }))
-        .pipe(gulp.dest('dist'));
+  gulp.src('src/*.js')
+    .pipe(babel({
+      presets: ['es2015'],
+    }))
+    .pipe(minify({
+      ext: {
+        src: '-debug.js',
+        min: '.js',
+      },
+    }))
+    .pipe(gulp.dest('dist'));
 });
 
 gulp.task('minify-css', function () {
-    return gulp.src('src/*.css')
-        .pipe(cleanCSS({compatibility: 'ie8'}))
-        .pipe(gulp.dest('dist'));
+  return gulp.src('src/*.css')
+    .pipe(cleanCSS({ compatibility: 'ie8' }))
+    .pipe(gulp.dest('dist'));
 });
